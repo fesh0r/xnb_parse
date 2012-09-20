7576e9f1c0dbcf9596dd27d32678c840e6bbdde5
fesh0r <dev@fesh.geek.nz>
2012-09-21 01:37:59 +1200

    tweak plugin finder a bit

--- xnb_parse/type_reader_manager.py
+++ xnb_parse/type_reader_manager.py
@@ -13,7 +13,7 @@ class TypeReaderManager(object):
     def __init__(self, reader_dir=None):
         self.reader_dir = reader_dir
         self.type_readers = {}
-        classes = _find_subclasses('xnb_parse', 'type_readers', BaseTypeReader)
+        classes = _find_subclasses('xnb_parse.type_readers', BaseTypeReader)
         print classes
 
     def get_type(self, name, version):
@@ -33,26 +33,18 @@ class BaseTypeReader(object):
         return self.name
 
 
-def _find_subclasses(root_pkgname, plugin_pkgname, cls):
-    """
-    Find all subclass of cls in py files located below path
-    (does look in sub directories)
-
-    @param cls: the base class that all subclasses should inherit from
-    @type cls: class
-    @rtype: list
-    @return: a list if classes that are subclasses of cls
-    """
-
+def _find_subclasses(pkgname, cls):
     subclasses = []
 
     # iccck, must be a better way of doing this
-    plugin_pkg = getattr(__import__(root_pkgname, globals(), locals(), [plugin_pkgname]), plugin_pkgname)
-    for _, modulename, _ in pkgutil.walk_packages(plugin_pkg.__path__, plugin_pkg.__name__ + '.'):
+    pkg = __import__(pkgname)
+    for d in pkgname.split('.')[1:]:
+        pkg = getattr(pkg, d)
+    for _, modulename, _ in pkgutil.walk_packages(pkg.__path__, pkg.__name__ + '.'):
         print 'searching', modulename
-        path_name, _, module_name = modulename.rpartition('.')
-        module = getattr(__import__(path_name, globals(), locals(), [module_name]), module_name)
-
+        module = __import__(modulename)
+        for d in modulename.split('.')[1:]:
+            module = getattr(module, d)
         # look through module dictionary for things that are subclass of cls but are not cls itself
         for key, entry in module.__dict__.items():
             if key == cls.__name__:
