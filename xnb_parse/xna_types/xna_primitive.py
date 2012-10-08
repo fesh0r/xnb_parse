349392029a82300a78049aea3650fa492e83a1c6
fesh0r <dev@fesh.geek.nz>
2012-10-08 13:05:30 +1300

    basic enum type

--- xnb_parse/xna_types/xna_primitive.py
+++ xnb_parse/xna_types/xna_primitive.py
@@ -1,3 +1,26 @@
 """
 primitive types
 """
+
+
+class Enum(object):
+    __slots__ = ('_value', '_name')
+    enum_values = None
+
+    def __init__(self, value):
+        self._value = value
+        _ = self.enum_values[value]
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def name(self):
+        return self.enum_values[self._value]
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return '%s(%d)' % (self.__class__.__name__, self._value)
