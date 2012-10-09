1bca4731584b8db1a5c39e94af524f99537c9859
fesh0r <dev@fesh.geek.nz>
2012-10-09 18:30:10 +1300

    use lxml instead of etree as it seems a bit less busted with escaping attributes and pretty printing

--- xnb_parse/file_formats/xml_utils.py
+++ xnb_parse/file_formats/xml_utils.py
@@ -2,34 +2,13 @@
 XML utils
 """
 
-import functools
-import xml.etree.cElementTree as ET
-
-
-class _E(object):
-    def __call__(self, tag, *children, **attrib):
-        elem = ET.Element(tag, attrib)
-        for item in children:
-            if isinstance(item, dict):
-                elem.attrib.update(item)
-            elif isinstance(item, basestring):
-                if len(elem):
-                    elem[-1].tail = (elem[-1].tail or "") + item
-                else:
-                    elem.text = (elem.text or "") + item
-            elif ET.iselement(item):
-                elem.append(item)
-            else:
-                raise TypeError("bad argument: %r" % item)
-        return elem
-
-    def __getattr__(self, tag):
-        return functools.partial(self, tag)
+import lxml.etree as ET
+from lxml.builder import ElementMaker
 
 
 # create factory object
-E = _E()
+E = ElementMaker()
 
 
 def output_xml(xml, filename):
-    ET.ElementTree(xml).write(filename, encoding='utf-8', xml_declaration=True)
+    ET.ElementTree(xml).write(filename, encoding='utf-8', xml_declaration=True, pretty_print=True)
