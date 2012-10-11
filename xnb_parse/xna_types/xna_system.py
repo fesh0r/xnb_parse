f01f7d8746736090e5da2cb751a8799e63da2b55
fesh0r <dev@fesh.geek.nz>
2012-10-11 15:40:35 +1300

    allow using attribute instead of text for entry in XNAList and XNADict xml output

--- xnb_parse/xna_types/xna_system.py
+++ xnb_parse/xna_types/xna_system.py
@@ -6,13 +6,17 @@ from xnb_parse.file_formats.xml_utils import E
 
 
 class XNAList(list):
-    def xml(self, xml_tag='List', xml_entry='Entry'):
+    def xml(self, xml_tag='List', xml_entry='Entry', attrib=None):
         root = E(xml_tag)
         for cur_value in self:
             if hasattr(cur_value, 'xml'):
-                root.append(cur_value.xml())
+                cur_tag = cur_value.xml()
+            elif attrib:
+                cur_tag = E(xml_entry)
+                cur_tag.set(attrib, unicode(cur_value))
             else:
-                root.append(E(xml_entry, unicode(cur_value)))
+                cur_tag = E(xml_entry, unicode(cur_value))
+            root.append(cur_tag)
         return root
 
     def export(self, _):
@@ -20,13 +24,21 @@ class XNAList(list):
 
 
 class XNADict(dict):
-    def xml(self, xml_tag='Dict', xml_entry='Entry'):
+    def xml(self, xml_tag='Dict', xml_entry='Entry', attrib=None):
         root = E(xml_tag)
         for cur_key, cur_value in self.items():
-            if hasattr(cur_value, 'xml'):
-                root.append(E(xml_entry, cur_value.xml(), key=unicode(cur_key)))
+            cur_tag = E(xml_entry)
+            if hasattr(cur_key, 'xml'):
+                raise ValueError("XNADict key is xml")
             else:
-                root.append(E(xml_entry, unicode(cur_value), key=unicode(cur_key)))
+                cur_tag.set('key', unicode(cur_key))
+            if hasattr(cur_value, 'xml'):
+                cur_tag.append(cur_value.xml())
+            elif attrib:
+                cur_tag.set(attrib, unicode(cur_value))
+            else:
+                cur_tag.text = unicode(cur_value)
+            root.append(cur_tag)
         return root
 
     def export(self, _):
