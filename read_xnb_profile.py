f39660c6877244207472e603e6716b9b79adfd0b
fesh0r <dev@fesh.geek.nz>
2012-10-13 02:41:31 +1300

    add profiled version of read_xnb, requires guppy

---
+++ read_xnb_profile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Dump info from XNB
+"""
+
+from xnb_parse.read_xnb_profile import main
+
+
+if __name__ == '__main__':
+    main()
---
+++ xnb_parse/read_xnb_profile.py
@@ -0,0 +1,37 @@
+"""
+Dump info from XNB
+"""
+
+import sys
+import os
+import time
+
+from guppy import hpy
+
+from xnb_parse.xnb_reader import XNBReader
+from xnb_parse.type_reader_manager import TypeReaderManager
+
+
+def read_xnb(in_file, type_reader_manager=None):
+    hp = hpy()
+    print in_file,
+    with open(in_file, 'rb') as in_handle:
+        in_data = in_handle.read()
+    hp.setrelheap()
+    xnb = XNBReader.load(in_data, type_reader_manager, parse=False)
+    print xnb,
+    print xnb.parse(),
+    xnb.export(os.path.join('../export', in_file.replace('.xnb', '')))
+    print 'done'
+    print hp.heap()
+
+
+def main():
+    if len(sys.argv) > 1:
+        totaltime = time.time()
+        type_reader_manager = TypeReaderManager()
+        for filename in sys.argv[1:]:
+            read_xnb(os.path.normpath(filename), type_reader_manager)
+        print '> Done in %.2f seconds' % (time.time() - totaltime)
+    else:
+        print 'read_xnb.py file1.xnb ...'
