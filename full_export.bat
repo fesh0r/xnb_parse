18cc4947296065b5248609c9fc9e6542804e8fe3
fesh0r <dev@fesh.geek.nz>
2012-10-17 23:13:53 +1300

    fiddle with read_xnb_dir so it takes a content path and an optional export path add bat file that does a full unpack, decompress and export add export dirs to .hgignore

---
+++ full_export.bat
@@ -0,0 +1,20 @@
+call fez_unpack.bat Content\Essentials.pak out
+if %errorlevel% neq 0 goto error
+
+call fez_unpack.bat Content\Other.pak out
+if %errorlevel% neq 0 goto error
+
+call xnb_decomp.bat out out_u
+if %errorlevel% neq 0 goto error
+
+call read_xnb_dir.bat out export
+if %errorlevel% neq 0 goto error
+
+pause
+
+goto end
+
+:error
+echo BANG
+
+:end
--- xnb_parse/read_xnb_dir.py
+++ xnb_parse/read_xnb_dir.py
@@ -12,34 +12,45 @@ from xnb_parse.type_reader import ReaderError
 from xnb_parse.type_reader_manager import TypeReaderManager
 
 
-def read_xnb(in_dir, type_reader_manager=None):
+def read_xnb_dir(in_dir, out_dir=None, type_reader_manager=None):
+    in_dir = os.path.normpath(in_dir)
+    if out_dir is not None:
+        out_dir = os.path.normpath(out_dir)
     for path, _, filelist in os.walk(in_dir, followlinks=True):
         sub_dir = os.path.relpath(path, in_dir)
         for cur_file in fnmatch.filter(filelist, '*.xnb'):
-            in_file = os.path.normpath(os.path.join(in_dir, sub_dir, cur_file))
+            short_name = os.path.join(sub_dir, cur_file)
+            in_file = os.path.join(in_dir, short_name)
+            out_file = None
+            if out_dir is not None:
+                out_file = os.path.join(out_dir, short_name).replace('.xnb', '')
 
-            print in_file,
+            print short_name,
             with open(in_file, 'rb') as in_handle:
                 in_data = in_handle.read()
             xnb = XNBReader.load(in_data, type_reader_manager, parse=False)
             try:
                 xnb.parse()
-                xnb.export(os.path.join('../export', in_file.replace('.xnb', '')))
+                if out_file is not None:
+                    xnb.export(out_file)
                 print 'done'
             except ReaderError as ex:
-                print "ReaderError in '%s'" % in_file
+                print "ReaderError in '%s'" % short_name
                 print ex
             except Exception:
-                print "Unexpected error in '%s'" % in_file
+                print "Unexpected error in '%s'" % short_name
                 raise
 
 
 def main():
-    if len(sys.argv) > 1:
+    if 1 < len(sys.argv) <= 3:
         totaltime = time.time()
         type_reader_manager = TypeReaderManager()
-        for filename in sys.argv[1:]:
-            read_xnb(os.path.normpath(filename), type_reader_manager)
+        in_dir = sys.argv[1]
+        out_dir = None
+        if len(sys.argv) > 2:
+            out_dir = sys.argv[2]
+        read_xnb_dir(in_dir, out_dir, type_reader_manager)
         print '> Done in %.2f seconds' % (time.time() - totaltime)
     else:
-        print 'read_xnb.py file1.xnb ...'
+        print 'read_xnb_dir.py content_dir [export_dir]'
