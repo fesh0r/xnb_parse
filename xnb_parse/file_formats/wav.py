0baf8f84afdaf8b1416157d6ed5eab89ce54d840
fesh0r <dev@fesh.geek.nz>
2012-10-26 16:13:37 +1300

    use structs in wav file reading/writing

--- xnb_parse/file_formats/wav.py
+++ xnb_parse/file_formats/wav.py
@@ -2,6 +2,8 @@
 WAV file writer
 """
 
+from uuid import UUID
+
 from xnb_parse.type_reader import ReaderError
 from xnb_parse.binstream import BinaryWriter, BinaryReader
 
@@ -21,6 +23,10 @@ WAVE_FORMAT = {
 
 
 class PyWavWriter(object):
+    _waveformatex = 'H H I I H H'
+    _waveformat_xma2 = 'H I I I I I I I B B H'
+    _waveformat_extensible = 'H I 16s'
+
     def __init__(self, header, data, dpds=None, seek=None, needs_swap=False):
         self.header_raw = header
         self.data_raw = data
@@ -29,64 +35,53 @@ class PyWavWriter(object):
         self.needs_swap = needs_swap
 
         h_s = BinaryReader(self.header_raw, needs_swap)
-        self.h_format_tag = h_s.read_uint16()
-        self.h_channels = h_s.read_uint16()
-        self.h_samples_per_sec = h_s.read_uint32()
-        self.h_avg_bytes_per_sec = h_s.read_uint32()
-        self.h_block_align = h_s.read_uint16()
-        self.h_bits_per_sample = h_s.read_uint16()
-
-        self.header_size = 2 + 2 + 4 + 4 + 2 + 2
+        waveformatex_size = h_s.calc_size(self._waveformatex)
+        (self.h_format_tag, self.h_channels, self.h_samples_per_sec, self.h_avg_bytes_per_sec, self.h_block_align,
+         self.h_bits_per_sample) = h_s.unpack(self._waveformatex)
+        header_size = waveformatex_size
 
         # do we have an WAVEFORMATEX
         self.h_size = None
-        if len(self.header_raw) >= 18:
+        if len(self.header_raw) >= waveformatex_size + 2:
             self.h_size = h_s.read_uint16()
-
-            self.header_size += 2
+            header_size += 2
 
             if self.h_format_tag == WAVE_FORMAT_XMA2:
-                if self.h_size != 34:
+                waveformat_xma2_size = h_s.calc_size(self._waveformat_xma2)
+                if self.h_size != waveformat_xma2_size:
                     raise ReaderError("Unknown cbSize for XMA2WAVEFORMATEX: %d" % self.h_size)
-                self.hx_num_streams = h_s.read_uint16()
-                self.hx_channel_mask = h_s.read_uint32()
-                self.hx_samples_encoded = h_s.read_uint32()
-                self.hx_bytes_per_block = h_s.read_uint32()
-                self.hx_play_begin = h_s.read_uint32()
-                self.hx_play_length = h_s.read_uint32()
-                self.hx_loop_begin = h_s.read_uint32()
-                self.hx_loop_length = h_s.read_uint32()
-                self.hx_loop_count = h_s.read_byte()
-                self.hx_encoder_version = h_s.read_byte()
-                self.hx_block_count = h_s.read_uint16()
-                self.header_size += 2 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 1 + 1 + 2
+                (self.hx_num_streams, self.hx_channel_mask, self.hx_samples_encoded, self.hx_bytes_per_block,
+                 self.hx_play_begin, self.hx_play_length, self.hx_loop_begin, self.hx_loop_length, self.hx_loop_count,
+                 self.hx_encoder_version, self.hx_block_count) = h_s.unpack(self._waveformat_xma2)
+                header_size += waveformat_xma2_size
             elif self.h_format_tag == WAVE_FORMAT_EXTENSIBLE:
-                if self.h_size < 22:
+                waveformat_extensible_size = h_s.calc_size(self._waveformat_extensible)
+                if self.h_size < waveformat_extensible_size:
                     raise ReaderError("Invalid cbSize for WAVEFORMATEXTENSIBLE: %d", self.h_size)
-                self.he_valid_bits_per_sample = h_s.read_uint16()
-                self.he_channel_mask = h_s.read_uint32()
-                self.he_subformat = h_s.read_bytes(16)
-                self.header_size += 2 + 4 + 16
+                (self.he_valid_bits_per_sample, self.he_channel_mask,
+                 he_subformat_bytes) = h_s.unpack(self._waveformat_extensible)
+                self.he_subformat = UUID(bytes_le=he_subformat_bytes)
+                header_size += waveformat_extensible_size
                 self.he_remainder = None
-                if self.h_size > 22:
-                    self.he_remainder = h_s.read_bytes(self.h_size - 22)
-                    self.header_size += self.h_size - 22
+                if self.h_size > waveformat_extensible_size:
+                    self.he_remainder = h_s.read_bytes(self.h_size - waveformat_extensible_size)
+                    header_size += self.h_size - waveformat_extensible_size
                     raise ReaderError("Extra bytes in WAVEFORMATEXTENSIBLE: %d", h_s.remaining)
             self.h_remainder = None
             if h_s.remaining():
                 self.h_remainder = h_s.remainder()
-                self.header_size += len(self.h_remainder)
+                header_size += len(self.h_remainder)
                 raise ReaderError("Trailing bytes in header: %d", len(self.h_remainder))
-        if self.header_size != len(self.header_raw):
-            raise ReaderError("Header size mismatch: %d != %d" % (self.header_size, len(self.header_raw)))
+        if header_size != len(self.header_raw):
+            raise ReaderError("Header size mismatch: %d != %d" % (header_size, len(self.header_raw)))
 
     def dump(self):
         if self.needs_swap:
             out_str = 'RIFX '
         else:
             out_str = 'RIFF '
-        out_str += 'h:%d hp:%d d:%d %s\n' % (len(self.header_raw), self.header_size, len(self.data_raw),
-                                             WAVE_FORMAT.get(self.h_format_tag, 'UNKNOWN'))
+        out_str += 'h:%d d:%d %s\n' % (len(self.header_raw), len(self.data_raw),
+                                       WAVE_FORMAT.get(self.h_format_tag, 'UNKNOWN'))
         out_str += 'wFormatTag:0x%04x nChannels:%d nSamplesPerSec:%d nAvgBytesPerSec:%d\n' % (
             self.h_format_tag, self.h_channels, self.h_avg_bytes_per_sec, self.h_samples_per_sec)
         out_str += 'nBlockAlign:%d wBitsPerSample:%d cbSize:%d\n' % (
@@ -105,34 +100,22 @@ class PyWavWriter(object):
 
     def write(self, filename):
         h_s = BinaryWriter()
-        h_s.write_uint16(self.h_format_tag)
-        h_s.write_uint16(self.h_channels)
-        h_s.write_uint32(self.h_samples_per_sec)
-        h_s.write_uint32(self.h_avg_bytes_per_sec)
-        h_s.write_uint16(self.h_block_align)
-        h_s.write_uint16(self.h_bits_per_sample)
+        h_s.pack(self._waveformatex, self.h_format_tag, self.h_channels, self.h_samples_per_sec,
+                 self.h_avg_bytes_per_sec, self.h_block_align, self.h_bits_per_sample)
         if self.h_size is not None:
             h_s.write_uint16(self.h_size)
             if self.h_format_tag == WAVE_FORMAT_XMA2:
-                h_s.write_uint16(self.hx_num_streams)
                 # hack so mono sounds end up center rather than left
-                if self.h_channels == 1:
-                    h_s.write_uint32(0)
+                if self.h_channels == 1 and self.hx_channel_mask == 1:
+                    hx_channel_mask = 0
                 else:
-                    h_s.write_uint32(self.hx_channel_mask)
-                h_s.write_uint32(self.hx_samples_encoded)
-                h_s.write_uint32(self.hx_bytes_per_block)
-                h_s.write_uint32(self.hx_play_begin)
-                h_s.write_uint32(self.hx_play_length)
-                h_s.write_uint32(self.hx_loop_begin)
-                h_s.write_uint32(self.hx_loop_length)
-                h_s.write_byte(self.hx_loop_count)
-                h_s.write_byte(self.hx_encoder_version)
-                h_s.write_uint16(self.hx_block_count)
+                    hx_channel_mask = self.hx_channel_mask
+                h_s.pack(self._waveformat_xma2, self.hx_num_streams, hx_channel_mask, self.hx_samples_encoded,
+                         self.hx_bytes_per_block, self.hx_play_begin, self.hx_play_length, self.hx_loop_begin,
+                         self.hx_loop_length, self.hx_loop_count, self.hx_encoder_version, self.hx_block_count)
             elif self.h_format_tag == WAVE_FORMAT_EXTENSIBLE:
-                h_s.write_uint16(self.he_valid_bits_per_sample)
-                h_s.write_uint32(self.he_channel_mask)
-                h_s.write_bytes(self.he_subformat)
+                h_s.pack(self._waveformat_extensible, self.he_valid_bits_per_sample, self.he_channel_mask,
+                         self.he_subformat.bytes_le)
                 if self.he_remainder:
                     h_s.write_bytes(self.he_remainder)
             if self.h_remainder:
