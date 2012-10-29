8c874d62c53d59b5323cef2b3ec8947797f72594
fesh0r <dev@fesh.geek.nz>
2012-10-29 23:28:52 +1300

    add XACT conversion to full_export output current step

--- full_export.bat
+++ full_export.bat
@@ -1,18 +1,27 @@
+echo Unpacking Essentials.pak...
 call fez_unpack.bat Content\Essentials.pak out
 if %errorlevel% neq 0 goto error
 
+echo Unpacking Other.pak...
 call fez_unpack.bat Content\Other.pak out
 if %errorlevel% neq 0 goto error
 
+echo Decompressing XNBs...
 call xnb_decomp.bat out out_u
 if %errorlevel% neq 0 goto error
 
+echo Converting XNBs...
 call read_xnb_dir.bat out_u export
 if %errorlevel% neq 0 goto error
 
+echo Converting XWB...
+call read_xact.bat "Content\Music\Wave Bank.xwb" export\Music
+
+echo Decoding XMAs...
 call xma_decode.bat export
 if %errorlevel% neq 0 goto error
 
+echo Decoding xWMAs...
 call xwma_decode.bat export
 if %errorlevel% neq 0 goto error
 
