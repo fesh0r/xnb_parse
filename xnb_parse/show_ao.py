5a1fb93ae8e3cb1bd01f0bad23bf2cceb77b6453
Andrew McRae <andrew@staff.inspire.net.nz>
2015-02-17 15:21:20 +1300

    directory support for show_ao
    fall back to directory based content manager if fez pakfile content
    manager fails, this allows passing the out directory instead of the
    Content directory as the first argument

--- xnb_parse/show_ao.py
+++ xnb_parse/show_ao.py
@@ -10,6 +10,8 @@ from pyglet.gl import *  # pylint: disable-msg=W0614,W0401
 
 from xnb_parse.fez_content_manager import FezContentManager
 from xnb_parse.trackball_camera import TrackballCamera, norm1, vec_args
+from xnb_parse.type_reader import ReaderError
+from xnb_parse.xna_content_manager import ContentManager
 from xnb_parse.xna_types.xna_math import Vector3
 
 
@@ -184,8 +186,12 @@ def main():
             except pyglet.window.NoSuchConfigException:
                 template = pyglet.gl.Config()
                 config = screen.get_best_config(template)
-        content_manager = FezContentManager(sys.argv[1])
+        # try and use FezContentManager if it works, failing back to directory reader
+        try:
+            content_manager = FezContentManager(sys.argv[1])
+        except ReaderError:
+            content_manager = ContentManager(sys.argv[1])
         AOWindow(content_manager=content_manager, asset_name=sys.argv[2], config=config)
         pyglet.app.run()
     else:
-        print('show_ao.py Content objectao')
+        print('show_ao.py Content|out objectao')
